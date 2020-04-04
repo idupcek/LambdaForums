da0331358a31bebef808aa2ea263fcf36f23e5ac
idupcek <ivan.dupcek@gmail.com>
2020-04-04 13:17:42 +1300

    Implement reply, start work on forum create

---
+++ LambdaForums/Controllers/ReplyController.cs
@@ -0,0 +1,56 @@
+﻿using LambdaForums.Data.Models;
+using LambdaForums.Models.Reply;
+using LambdaForums.Service;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Controllers
+{
+    public class ReplyController : Controller
+    {
+        private readonly PostService _postService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ReplyController(
+            PostService postService, 
+            UserManager<ApplicationUser> userManager)
+        {
+            _postService = postService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Create (int id)
+        {
+            var post = _postService.GetById(id);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var model = new PostReplyModel
+            {
+                PostContent = post.Content,
+                PostTitle = post.Title,
+                PostId = post.Id,
+
+                AuthorId = user.Id,
+                AuthorName = User.Identity.Name, 
+                AuthorImageUrl = user.ProfileImageUrl,
+                AuthorRating = user.Rating,
+                IsAuthorAdmin = User.IsInRole("Admin"),
+
+                ForumName = post.Forum.Title,
+                ForumId = post.Forum.Id,
+                ForumImageUrl = post.Forum.ImageUrl,
+
+                Created = DateTime.Now,
+                
+
+            };
+
+            return View(model);
+        }
+    }
+}
